a6e2967ec7beb541b462e38ddefb6bf66845b2dc
Ashiqur <ashiqur2812@gmail.com>
2025-10-22 09:21:11 +0600

    Changes the functionality in the banner

--- src/Components/Banner/Banner.jsx
+++ src/Components/Banner/Banner.jsx
@@ -1,18 +1,17 @@
 import { useLocation, useParams } from "react-router-dom";
 import bannerImage from "../../assets/banner.jpg";
+
 const Banner = () => {
   const { category } = useParams();
   const { pathname } = useLocation();
-  {
-    pathname === `/category/${category}` ? "mb-96" : "";
-  }
+  { pathname === `/category/${category}` ? "mb-96" : ""}
+
   return (
     <>
       <div className="mt-12">
         <button className="btn mx-2 lg:mx-[44rem] text-purple-700  rounded-full mb-8 ml-40 lg:ml-[43rem]">
           Shop now
         </button>
-
         <img
           className="border-2 p-5 max-w-5xl w-60 lg:w-full  mx-auto rounded-2xl"
           src={bannerImage}
